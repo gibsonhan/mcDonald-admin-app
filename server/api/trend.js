ffc953780e2054cca77c35b68e28172a5b3b32d0
Gibson <gibsonxhan@gmail.com>
2020-10-06 19:58:36 -0700

    WIP backend trend card implementation

--- server/api/trend.js
+++ server/api/trend.js
@@ -2,13 +2,13 @@ const express = require('express');
 const trendRoutes = express.Router();
 
 //require controller modules
-const controller = require('../controller/trend');
+const trend = require('../controller/trend');
 
 // CRUD for company profile
 trendRoutes.route('/').get(trend.list);
-trendRoutes.route('/:id').get(trend.id);
+trendRoutes.route('/:id').get(trend.single);
 trendRoutes.route('/').post(trend.create);
 trendRoutes.route('/:id').put(trend.update);
-trendRoutes.route('/:id').delete(trend.delete);
+trendRoutes.route('/:id').delete(trend.remove);
 
 module.exports = trendRoutes;
